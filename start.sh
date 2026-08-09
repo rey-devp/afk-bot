19da5a036ef707eb2a6de69147e7b5e10c9612b4
rey-devp <rey.devp@gmail.com>
2026-08-09 14:16:46 +0700

    feat: add startup script to initialize provider server and execute bot binary

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+
+# Jalankan PO Token provider di background, listen di localhost:4416 (default)
+node /opt/bgutil-provider/server/build/main.js &
+
+# Tunggu sebentar supaya provider server siap sebelum bot mulai
+sleep 2
+
+# Jalankan bot Go utama (foreground, PID 1)
+exec ./bot-afk
